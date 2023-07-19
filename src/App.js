8619f242c365cebeec00e67614083882cde8e3d5
Giada15 <tascheriogiada@gmail.com>
2023-07-19 10:48:25 -0400

    Add sorting items feature

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function Form({ onAddItems }) {
     if (!description) return
 
     const newItem = { description, quantity, packed: false, id: Date.now() }
-    console.log(newItem)
+    // console.log(newItem)
 
     onAddItems(newItem)
 
@@ -80,10 +80,26 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input")
+
+  let sortedItems
+
+  if (sortBy === "input") sortedItems = items
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description))
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed))
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -92,6 +108,14 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   )
 }
